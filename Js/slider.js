c0ea6798ea693088bd40b099f80acfa604115c17
Anthony Monsalve <22anthony.monsalve@gmail.com>
2018-11-09 11:31:43 -0400

    Slider telefono

--- Js/slider.js
+++ Js/slider.js
@@ -1,23 +1,14 @@
 $('.center-slider').slick({
   centerMode: true,
   infinite: true,
-  centerPadding: '60px',
-  slidesToShow: 3,
+  centerPadding: '0px',
+  slidesToShow: 5,
   speed: 500,
   arrows: false,
   autoplay: true,
   autoplaySpeed: 4500,
   variableWidth: false,
   responsive: [{
-      breakpoint: 480,
-      settings: {
-        arrows: false,
-        centerMode: true,
-        centerPadding: '80px',
-        slidesToShow: 1
-      }
-    },
-    {
       breakpoint: 480,
       settings: {
         arrows: false,
