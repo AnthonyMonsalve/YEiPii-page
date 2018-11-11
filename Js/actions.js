85b4da0272a49091d05e181caa9a9808baeb3806
Anthony Monsalve <22anthony.monsalve@gmail.com>
2018-11-10 22:26:22 -0400

    Terminado el peo del menu que talfi

--- Js/actions.js
+++ Js/actions.js
@@ -1,68 +1,157 @@
 /*NAV ANIMACION*/
-		window.onscroll = function(){
-			AddOrRemove()
-		};
+window.onscroll = function () {
+	AddOrRemove();
+	removeActiveMenuResponsive();
+};
 
-		var Header = document.getElementById("header");
-		Logo = document.getElementById("logo");
-		/*var BurguerButton = document.getElementById("burguerButton");*/
-		var Nav = document.getElementById("Nav");
-		
-		var HeaderPosition = Header.offsetTop;
+var Header = document.getElementById("header");
+Logo = document.getElementById("logo");
+/*var BurguerButton = document.getElementById("burguerButton");*/
+var Nav = document.getElementById("Nav");
 
-		function AddOrRemove(){
-			if(window.pageYOffset >= HeaderPosition){
-				Header.classList.add("header-down");
-				Logo.src="images/logo/YEiPii.png";/*
-				BurguerButton.classList.add("burguer-blue");*/
-				Nav.classList.add("nav-down");
-			}
-			else{
-				Header.classList.remove("header-down");
-				Logo.src="images/logo/YEiPii.png";/*
-				BurguerButton.classList.remove("burguer-blue");*/
-				Nav.classList.remove("nav-down");
-			}
-		}
+var HeaderPosition = Header.offsetTop;
+
+function AddOrRemove() {
+	if (window.pageYOffset >= HeaderPosition) {
+		Header.classList.add("header-down");
+		Logo.src = "images/logo/YEiPii.png";
+		/*
+						BurguerButton.classList.add("burguer-blue");*/
+		Nav.classList.add("nav-down");
+	} else {
+		Header.classList.remove("header-down");
+		Logo.src = "images/logo/YEiPii.png";
+		/*
+						BurguerButton.classList.remove("burguer-blue");*/
+		Nav.classList.remove("nav-down");
+	}
+}
 
 /*SCROLL REVEAL*/
-		// Changing the defaults
-		window.sr = ScrollReveal({ reset: false });
+// Changing the defaults
+window.sr = ScrollReveal({
+	reset: false
+});
 
-		if(window.matchMedia( "(max-width: 840px)" ).matches){
-			// Customizing a reveal set
-			sr.reveal('.animate', { duration: 750, distance: '60px', origin: 'bottom', delay: 250 });
-			sr.reveal('.animate.slow', { duration: 750, distance: '30px', origin: 'bottom', delay: 600 });
-			
-			sr.reveal('.animate-right', { duration: 750, distance: '60px', origin: 'bottom', delay: 250 });
-			sr.reveal('.animate-right.slow', { duration: 750, distance: '30px', origin: 'bottom', delay: 600 });	
-		}else{
-			// Customizing a reveal set
-			sr.reveal('.animate', { duration: 750, distance: '60px', origin: 'left', delay: 250 });
-			sr.reveal('.animate.slow', { duration: 750, distance: '60px', origin: 'left', delay: 600 });
-			sr.reveal('.animate.btn-slider', { duration: 800, distance: '60px', origin: 'bottom', delay: 450 });
-			
-			sr.reveal('.animate-right', { duration: 750, distance: '60px', origin: 'right', delay: 250 });
-			sr.reveal('.animate-right.slow', { duration: 750, distance: '60px', origin: 'right', delay: 600 });			
-		}
+if (window.matchMedia("(max-width: 840px)").matches) {
+	// Customizing a reveal set
+	sr.reveal('.animate', {
+		duration: 750,
+		distance: '60px',
+		origin: 'bottom',
+		delay: 250
+	});
+	sr.reveal('.animate.slow', {
+		duration: 750,
+		distance: '30px',
+		origin: 'bottom',
+		delay: 600
+	});
+
+	sr.reveal('.animate-right', {
+		duration: 750,
+		distance: '60px',
+		origin: 'bottom',
+		delay: 250
+	});
+	sr.reveal('.animate-right.slow', {
+		duration: 750,
+		distance: '30px',
+		origin: 'bottom',
+		delay: 600
+	});
+} else {
+	// Customizing a reveal set
+	sr.reveal('.animate', {
+		duration: 750,
+		distance: '60px',
+		origin: 'left',
+		delay: 250
+	});
+	sr.reveal('.animate.slow', {
+		duration: 750,
+		distance: '60px',
+		origin: 'left',
+		delay: 600
+	});
+	sr.reveal('.animate.btn-slider', {
+		duration: 800,
+		distance: '60px',
+		origin: 'bottom',
+		delay: 450
+	});
+
+	sr.reveal('.animate-right', {
+		duration: 750,
+		distance: '60px',
+		origin: 'right',
+		delay: 250
+	});
+	sr.reveal('.animate-right.slow', {
+		duration: 750,
+		distance: '60px',
+		origin: 'right',
+		delay: 600
+	});
+}
 
 
 /* ACCIONES DEL MENU RESPONSIVE */
 
-function menuResponsive (){
+function menuResponsive() {
 	document.getElementById('contentNav').classList.toggle('active');
 	//document.getElementById('button-menu').classList.toggle('active');
 	document.getElementById('button-menu').classList.toggle('is-active');
 }
 
-function footerRedesDown (){
+
+//almacenando el div y el boton en unas variables
+var contentNav = document.getElementById('contentNav');
+var but = document.getElementById('button-menu');
+
+var clickEvent = (function() {
+	if ('ontouchstart' in document.documentElement === true)
+	  return 'touchstart';
+	else
+	  return 'click';
+  })();
+
+//funcion para cualquier clic en el documento
+document.addEventListener(clickEvent, function (e) {
+
+	//obtiendo informacion del DOM para  
+	var clic = e.target;
+
+	if (!(contentNav.classList.contains("active")) && (clic.classList.contains("menu-open"))) {
+		menuResponsive();
+		//console.log(clic)
+	} else if (contentNav.classList.contains("active") && (!(clic.classList.contains("cerrar")))) {
+		document.getElementById('contentNav').classList.remove('active');
+		//document.getElementById('button-menu').classList.remove('active');
+		document.getElementById('button-menu').classList.remove('is-active');
+		//console.log(clic);
+		//console.log(contentNav);
+	}
+}, false);
+
+function removeActiveMenuResponsive() {
+	document.getElementById('button-menu').classList.remove('is-active');
+	document.getElementById('contentNav').classList.remove('active');
+}
+
+
+
+
+/* RESPONSIVE FOOTER */
+
+function footerRedesDown() {
 	let redes = document.getElementById('acordeon-redes');
 	let app = document.getElementById('acordeon-app');
 	let ayuda = document.getElementById('acordeon-ayuda');
 
 	redes.classList.toggle('fa-chevron-down');
 
-	if(app.classList.contains("fa-chevron-down") || redes.classList.contains("fa-chevron-down")){
+	if (app.classList.contains("fa-chevron-down") || redes.classList.contains("fa-chevron-down")) {
 		app.classList.remove('fa-chevron-down');
 		app.classList.add('fa-chevron-right');
 		ayuda.classList.remove('fa-chevron-down');
@@ -70,14 +159,14 @@ function footerRedesDown (){
 	}
 }
 
-function footerAyudaDown (){
+function footerAyudaDown() {
 	let redes = document.getElementById('acordeon-redes');
 	let app = document.getElementById('acordeon-app');
 	let ayuda = document.getElementById('acordeon-ayuda');
 
 	ayuda.classList.toggle('fa-chevron-down');
 
-	if(app.classList.contains("fa-chevron-down") || redes.classList.contains("fa-chevron-down")){
+	if (app.classList.contains("fa-chevron-down") || redes.classList.contains("fa-chevron-down")) {
 		app.classList.remove('fa-chevron-down');
 		app.classList.add('fa-chevron-right');
 		redes.classList.remove('fa-chevron-down');
@@ -86,49 +175,17 @@ function footerAyudaDown (){
 }
 
 
-function footerAppDown (){
+function footerAppDown() {
 	let redes = document.getElementById('acordeon-redes');
 	let app = document.getElementById('acordeon-app');
 	let ayuda = document.getElementById('acordeon-ayuda');
-	
+
 	app.classList.toggle('fa-chevron-down');
 
-	if(redes.classList.contains("fa-chevron-down") || ayuda.classList.contains("fa-chevron-down")){
+	if (redes.classList.contains("fa-chevron-down") || ayuda.classList.contains("fa-chevron-down")) {
 		redes.classList.remove('fa-chevron-down');
 		redes.classList.add('fa-chevron-right');
 		ayuda.classList.remove('fa-chevron-down');
 		ayuda.classList.add('fa-chevron-right');
 	}
 }
-
-/* CATEGORÍAS BUTTONS */
-
-const slider = document.querySelector('.items');
-let isDown = false;
-let startX;
-let scrollLeft;
-
-slider.addEventListener('mousedown', (e) => {
-    isDown = true;
-    slider.classList.add('active');
-    startX = e.pageX - slider.offsetLeft;
-    scrollLeft = slider.scrollLeft;
-});
-
-slider.addEventListener('mouseleave', () => {
-    isDown = false;
-    slider.classList.remove('active');
-});
-
-slider.addEventListener('mouseup', () => {
-    isDown = false;
-    slider.classList.remove('active');
-});
-
-slider.addEventListener('mousemove', (e) => {
-    if (!isDown) return; 
-    e.preventDefault();
-    const x = e.pageX - slider.offsetLeft;
-    const walk = (x - startX) * 3;
-    slider.scrollLeft = scrollLeft - walk;
-});
