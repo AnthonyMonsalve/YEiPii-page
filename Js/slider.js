3acfbb77603fa1c18bbaf34a214b2cd2d345e020
Anthony Monsalve <22anthony.monsalve@gmail.com>
2019-02-26 22:21:39 -0400

    Listo carajo

--- Js/slider.js
+++ Js/slider.js
@@ -75,17 +75,3 @@ $('.center-slider').slick({
     }
   ]
 });
-
-
-
-function myFunction(x) {
-  if (x.matches) { // If media query matches
-    document.getElementById("cinex").src="images/slider/fotos/9-small.png"
-  } else {
-    document.getElementById("cinex").src="images/slider/fotos/9.png"
-  }
-}
-
-var x = window.matchMedia("(max-width: 700px)")
-myFunction(x) // Call listener function at run time
-x.addListener(myFunction) // Attach listener function on state changes
